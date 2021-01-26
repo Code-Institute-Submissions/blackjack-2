db05b0ae8c45113f0d60ec6c22819d1c9c7877ec
bowets <bforsek@gmail.com>
2021-01-26 23:50:31 +0000

    hit button now displays new cards in empty placeholders

--- assets/js/startGame.js
+++ assets/js/startGame.js
@@ -1,6 +1,9 @@
 let fullDeck = [];
 let deckReady = false;
-let playerScore, computerScore = 0; 
+let playerScore, computerScore = 0;
+let player = "human";
+let computerHand = [];
+let playerHand = [];
 
 
 
@@ -12,10 +15,10 @@ function startGame() {
             this.type = type;
             this.image = image_url;
         }
-        
+
         let cardValue = 1;
         for (let i = 1; i < 14; i++) {
-            if (i > 10){
+            if (i > 10) {
                 cardValue = 10;
             }
             for (let j = 0; j < 4; j++) {
@@ -29,44 +32,38 @@ function startGame() {
                     fullDeck.push(new Card(cardValue, "clubs", `assets/img/cards/${i}C.jpg`));
                 }
             }
-            cardValue = i+1;
+            cardValue = i + 1;
         }
-        console.log(fullDeck);
+
         // after the deck of cards is created, the cards are dealt. The computer and player each get two cards from the deck
-        let computerHand = [];
-        let playerHand = [];
-        
-        for (let i = 0; i < 4; i++){
+
+
+        for (let i = 0; i < 4; i++) {
             let cardNumber = Math.floor((Math.random() * fullDeck.length));
-            if(i<2) {
+            if (i < 2) {
                 computerHand.push(fullDeck[cardNumber]);
             } else {
                 playerHand.push(fullDeck[cardNumber]);
             }
             fullDeck.splice(cardNumber, 1);
-            console.log(cardNumber);
-            console.log(fullDeck);
+
         }
-        console.log(computerHand);
-        console.log(playerHand);
-        
+
+
 
         document.getElementById("options-notification").innerHTML = '';
 
-        // computer gets two cards
-        document.getElementById("computer-card-1").innerHTML = `<img src="assets/img/cards/blue_back.jpg" alt="blue card back" id="computer-card-played-1" width="100%" height="100%">`
-        document.getElementById("computer-card-2").innerHTML = `<img src="${computerHand[1].image}" alt="${computerHand[1].value} + ' ' + ${computerHand[1].type}" id="computer-card-played-2" width="100%" height="100%">`
+        // computer gets two cards and human gets two cards at the beginning of the game
+
         computerScore = calculateScore(computerHand);
-        // player gets two cards
-        document.getElementById("human-card-1").innerHTML = `<img src="${playerHand[0].image}" alt="${playerHand[0].value} + ' ' + ${playerHand[0].type}" id="human-card-played-1" width="100%" height="100%">`
-        document.getElementById("human-card-2").innerHTML = `<img src="${playerHand[1].image}" alt="${playerHand[1].value} + ' ' + ${playerHand[1].type}" id="human-card-played-2" width="100%" height="100%">`
         playerScore = calculateScore(playerHand);
-        console.log(fullDeck);
-        
+
+
+        displayCards(playerHand, computerHand, player);
+
+
         // player score is displayed, but computer score is hidden for now
-        document.getElementById("human-score").innerText = playerScore;
-        console.log(computerScore);
-        console.log(playerScore);
+ 
 
         deckReady = true;
     } else {
@@ -86,11 +83,13 @@ function hit(deck) {
         return;
     }
     let cardNumber = Math.floor((Math.random() * deck.length));
-    document.getElementById("human-score").innerHTML = deck[cardNumber].value + " " + deck[cardNumber].type;
-    document.getElementById("human-card-played-1").src = deck[cardNumber].image;
+    playerHand.push(deck[cardNumber]);
+
+    
     deck.splice(cardNumber, 1);
-    console.log(cardNumber);
-    console.log(deck);
+    displayCards(playerHand, computerHand, player);
+    playerScore = calculateScore(playerHand);
+
 
 }
 
@@ -99,15 +98,41 @@ function hit(deck) {
 function calculateScore(score) {
     let totalScore = 0;
     for (let i = 0; i < score.length; i++) {
-            
+
         totalScore += score[i].value;
 
         // if one of the cards is an Ace it can be either 1 or 11. This will recalculate the score if the higher score would be less than 22
-        if(score[i].value == 1 && totalScore+10 < 22) {
+        if (score[i].value == 1 && totalScore + 10 < 22) {
             totalScore += 10;
         }
     }
 
 
     return totalScore;
+}
+
+function displayCards(playerHand, computerHand, player) {
+    if (player == "human") {
+
+        document.getElementById("computer-card-1").innerHTML = `<img src="assets/img/cards/blue_back.jpg" alt="blue card back" id="computer-card-played-1" width="100%" height="100%">`;
+        document.getElementById("computer-card-2").innerHTML = `<img src="${computerHand[1].image}" alt="${computerHand[1].value} + ' ' + ${computerHand[1].type}" id="computer-card-played-2" width="100%" height="100%">`;
+
+        for (let i = 0; i < playerHand.length; i++) {
+            document.getElementById(`human-card-${i + 1}`).innerHTML = `<img src="${playerHand[i].image}" alt="${playerHand[i].value} + ' ' + ${playerHand[i].type}" id="human-card-played-${i + 1}" width="100%" height="100%">`;
+        }
+        playerScore = calculateScore(playerHand);
+        document.getElementById("human-score").innerText = playerScore;
+
+    } else {
+
+        for (let i = 0; i < playerHand.length; i++) {
+            document.getElementById(`human-card-${i + 1}`).innerHTML = `<img src="${playerHand[i].image}" alt="${playerHand[i].value} + ' ' + ${playerHand[i].type}" id="human-card-played-${i + 1}" width="100%" height="100%">`;
+
+        }
+
+        for (let i = 0; i < computerHand.length; i++) {
+            document.getElementById(`computer-card-${i + 1}`).innerHTML = `<img src="${computerHand[i].image}" alt="${computerHand[i].value} + ' ' + ${computerHand[i].type}" id="computer-card-played-2" width="100%" height="100%">`;
+
+        }
+    }
 }
